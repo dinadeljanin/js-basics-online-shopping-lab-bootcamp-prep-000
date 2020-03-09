1ba6eba28ae3729a3579d7792c2dcf0124cedd3d
D D <ddeljanin5@icloud.com>
2020-03-09 00:48:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function viewCart() {
     if (list.length == 1) {
       return `In your cart, you have ${list[0]}.`;
     } else if (list.length > 2) {
-      return `In your cart, you have ${list.slice(0, list.length-1).join(', ')} and ${list[list.length-1]}.`;   
+      return `In your cart, you have ${list.slice(0, list.length-1).join(', ')}and ${list[list.length-1]}.`;   
      }
   }
 }
