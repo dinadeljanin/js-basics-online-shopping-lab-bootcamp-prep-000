1178d92c1024121d4e79edf59e0cf6948486cc34
D D <ddeljanin5@icloud.com>
2020-03-09 00:45:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,15 +25,15 @@ function viewCart() {
     return "Your shopping cart is empty.";
   } else {
    for (let i = 0; i < cart.length; i++) {
-     list.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+     list.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }
+    console.log(list);
     // bare with me here
-     if (list.length == 1) {
-       return `In your cart, you have ${list[0]}.`;
-     } else if (list.length > 2) {
-       return `In your cart, you have ${list.slice(0, list.length-1)} and ${list[list.length-1]}.`;
-       
+    if (list.length == 1) {
+      return `In your cart, you have ${list[0]}.`;
+    } else if (list.length > 2) {
+      return `In your cart, you have ${list.slice(0, list.length-1).join(', ')} and ${list[list.length-1]}.`;   
      }
-   }
   }
 }
 
