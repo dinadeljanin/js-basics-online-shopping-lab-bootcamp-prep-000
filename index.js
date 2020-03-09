331f356b8605805963e07186e571819e4292d86f
D D <ddeljanin5@icloud.com>
2020-03-09 01:48:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,5 +59,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber == undefined) {
     return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    cart.slice(0);
+    return `Your total cost is ${total()} which will be charged to the card ${cardNumber}.`;
   }
 }
