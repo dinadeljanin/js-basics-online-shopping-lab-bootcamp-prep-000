a50e4088cb8333a619d3f910f430f392ac6f1b0f
D D <ddeljanin5@icloud.com>
2020-03-09 00:29:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function viewCart() {
   } else {
    for (let i = 0; i < cart.length; i++) {
      list.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+    // bare with me here
      if (list.length == 1) {
        return `In your cart, you have ${list[0]}.`;
      } else if (list.length == 2) {
