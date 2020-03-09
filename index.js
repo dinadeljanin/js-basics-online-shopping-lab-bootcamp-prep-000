660e0d94b031dfb12cf3b9b944d0275435714457
D D <ddeljanin5@icloud.com>
2020-03-09 01:42:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,9 +50,10 @@ function removeFromCart(item) {
     if (item == cart[i].itemName) {
       cart.splice(i, 1);
       return cart;
+    } else {
+      return `That item is not in your cart.`;
     }
   }
-  return `That item is not in your cart.`;
 }
 
 
