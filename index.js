85dd66d4fa6a8dd4636b504f79be88f7b96ddafb
D D <ddeljanin5@icloud.com>
2020-03-09 01:51:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function placeOrder(cardNumber) {
   if (cardNumber == undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    cart.slice(0);
+    cart.slice(1);
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
